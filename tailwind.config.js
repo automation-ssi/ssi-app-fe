f078a1bb57dbf1ddf2d988a6e2b7049ae0dce9ac
markmakilan <markmakilan@gmail.com>
2024-09-05 15:19:19 +0800

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#fafafa',
+        secondary: '#d4d4d8',
+        accent: '#18181b'
+      }
+    }
   },
   plugins: []
 }
